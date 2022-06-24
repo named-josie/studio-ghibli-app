c10c10728613f52938b475a107a0f30e83e4546c
named.josie <named.josie@gmail.com>
2022-06-24 16:59:14 -0400

    ainfo in with pic in movies

--- src/Components/pages/Movies.js
+++ src/Components/pages/Movies.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
-import { Dropdown } from '../../Dropdown';
+import "./movies.css"
 export default class Movies extends Component {
   constructor() {
     super();
     this.state = {
-      collection: [],
-      value: '',
+      movies: [],
+      titleSelected: '',
     };
   }
   componentDidMount() {
     fetch('https://ghibliapi.herokuapp.com/films/')
       .then((response) => response.json())
-      .then((res) => this.setState({ collection: res }));
+      .then((res) => this.setState({ movies: res }));
   }
   onChange = (event) => {
-    this.setState({ value: event.target.value });
+    this.setState({ titleSelected: event.target.value });
   };
 
   render() {
+    let { movies, titleSelected } = this.state;
+
+    const movieTitle = movies.map((movie) => {
+      return <option key={movie.id}>{movie.title}</option>;
+    });
+
+    let selected = movies.find((movie) => movie.title === titleSelected);
+
     return (
       <div className='movies'>
-        <h2>Select a Movie</h2>
-        <Dropdown
-          name={this.state.title}
-          options={this.state.collection}
-          onChange={this.onChange}
-        />
+        <h1>Select a movie</h1>
+
+        <select onChange={this.onChange}>
+          <option></option>
+          {movieTitle}
+        </select>
+        <div>
+          {selected ? (
+            <div>
+              <h2>{selected.title}</h2>
+              <img className='title-image' src={selected.image}></img>
+              <p>
+                <strong>Release Date:</strong> {selected.release_date}
+              </p>
+              <p>
+                <strong>Description:</strong> {selected.description}
+              </p>
+            </div>
+          ) : null}
+        </div>
       </div>
     );
   }
