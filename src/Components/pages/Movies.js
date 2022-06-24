9a5feee3fed540fd9a70aa70521235af1826d17c
named.josie <named.josie@gmail.com>
2022-06-24 15:24:27 -0400

    added dropdown list

--- src/Components/pages/Movies.js
+++ src/Components/pages/Movies.js
@@ -1,7 +1,31 @@
-import React from 'react'
-
-export default function Movies() {
-  return (
-    <div>Movies</div>
-  )
-}
+import React, { Component } from 'react'
+import { Dropdown } from '../../Dropdown'
+export default class Movies extends Component {
+    constructor() {
+        super()
+        this.state = {
+          collection: [],
+          value: '',
+        }
+      }
+      componentDidMount() {
+        fetch('https://ghibliapi.herokuapp.com/films/')
+          .then((response) => response.json())
+          .then((res) => this.setState({ collection: res }))
+      }
+      onChange = (event) => {
+        this.setState({ value: event.target.value })
+      }
+      render() {
+        return (
+          <div className="movies">
+            <h2>Select a Movie</h2>
+            <Dropdown
+              name={this.state.title}
+              options={this.state.collection}
+              onChange={this.onChange}
+            />
+          </div>
+        )
+      }
+    }
---
+++ src/Dropdown.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+export const Dropdown = (props) => (
+  <div className="form-group">
+    <strong>{props.title}</strong>
+    <select
+      className="movie-titles"
+      name="{props.title}"
+      onChange={props.onChange}
+    >
+      <option defaultValue>Select {props.title}</option>
+      {props.options.map((item, index) => (
+        <option key={index} value={item.id}>
+          {item.title}
+        </option>
+      ))}
+    </select>
+  </div>
+)
